19d3734529f1fc2d227e23f1fbc27c00396971cc
Fjodor Naumenko <ggrowling@gmail.com>
2015-02-02 02:48:58 +0300

    Simple FTP client

---
+++ FtpConsoleClient/FtpClient.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace ftpConsoleClient
+{
+    // Ftp client class
+    // It consists of
+    //  - ftp server's uri
+    //  - username, password for login
+    //  - shell for basic ftp commands
+    class FtpClient
+    {
+        private string ftpUri = "ftp://ftp.mozilla.org/";
+        private FtpWebRequest request;
+        private NetworkCredential credentials;
+
+        // attributes of items received from ftp LIST method (unix: ls -l)
+        // only for apache? servers, IIS returns data in other format (probably?)
+        private enum ItemAttributes
+        {
+            TypeMod = 0,
+            RefCount = 1,
+            OwnerName = 2,
+            GroupName = 3,
+            Size = 4,
+            MonthStamp = 5,
+            DayStamp = 6,
+            TimeOrYearStamp = 7,
+            Name = 8,
+        }
+
+        // returns created ftp request
+        private FtpWebRequest CreateFtpRequest(string method, string uri)
+        {
+            FtpWebRequest requestInstance = (FtpWebRequest)WebRequest.Create(uri);
+            requestInstance.Method = method;
+            requestInstance.Credentials = credentials;
+
+            return requestInstance;
+        }
+
+
+        public FtpClient() { credentials = new NetworkCredential("", ""); }
+
+        public FtpClient(string uri)
+        {
+            ftpUri = uri == "" || uri == null ? ftpUri : "ftp://" + uri;
+            credentials = new NetworkCredential("", "");
+        }
+
+        public FtpClient(string uri, string username, string password)
+        {
+            ftpUri = uri == "" || uri == null ? ftpUri : "ftp://" + uri;
+            credentials = new NetworkCredential(username, password);
+        }
+
+        public string FtpUri 
+        { 
+            get { return ftpUri.Substring(6); } 
+            set { ftpUri = value == "" || value == null ? ftpUri : value; }
+        }
+
+        public void Reloggin(string username, string password)
+        {
+            credentials.UserName = username;
+            credentials.Password = password;
+        }
+
+        // all methods take variable length of params, but these params
+        //  are some attributes or keys for this function
+        public void ChangeDirectory(params string[] consoleArgs)
+        {
+            // this func takes 1 param only
+            if (consoleArgs.Length != 1)
+            {
+                Console.WriteLine("You must specify directory you want to move!");
+                return;
+            }
+
+            request = CreateFtpRequest(WebRequestMethods.Ftp.PrintWorkingDirectory, ftpUri + consoleArgs[0]);
+            using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+            {
+                if (ftpUri[ftpUri.Length - 1] != '/' && consoleArgs[0][0] != '/')
+                {
+                    Console.Write("Wrong path!\n\n");
+                    return;
+                }
+                ftpUri = response.ResponseUri.ToString();
+            }
+        }
+
+        public void DownloadAndSafeFile(string[] consoleArgs)
+        {
+            // this function takes not less than 2 params
+            if (consoleArgs.Length < 2)
+            {
+                Console.WriteLine("\tYou must specify file name and the path to safe!");
+                return;
+            }
+
+            request = CreateFtpRequest(WebRequestMethods.Ftp.DownloadFile, ftpUri + "/" + consoleArgs[0]);
+            Console.Write("Connecting to {0}...\n\n", ftpUri);
+
+            FtpWebResponse response = null;
+
+            try
+            {
+                response = (FtpWebResponse)request.GetResponse();
+            }
+            catch (System.Net.WebException e)
+            {
+                Console.Write("Error: Couldn't find file {0} at {1}\nShow full message? (y/n) ", consoleArgs[0], ftpUri);
+
+                if ('y' == (char)Console.Read()) Console.Write("{0}\n", e);
+
+                Console.WriteLine();
+                return;
+            }
+
+            using (response)
+            {
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(responseStream))
+                    {
+                        try
+                        {
+                            using (StreamWriter file = new StreamWriter(consoleArgs[1] + consoleArgs[0]))
+                            {
+                                // safe file to specified directory
+                                file.Write(reader.ReadToEnd());
+                                Console.Write("Download complete, status {0}", response.StatusDescription);
+                                Console.Write("File {0} successfully saved at {1}\n\n", consoleArgs[0], consoleArgs[1]);
+                            }
+                        }
+                        catch (System.IO.DirectoryNotFoundException)
+                        {
+                            Console.Write("Directory {0} not found!\n\n", consoleArgs[1] + consoleArgs[0]);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void DownloadAndShowFile(string[] consoleArgs)
+        {
+            // this func takes 1 param only too
+            try
+            {
+                request = CreateFtpRequest(WebRequestMethods.Ftp.DownloadFile, ftpUri + "/" + consoleArgs[0]);
+            }
+            catch (System.IndexOutOfRangeException)
+            {
+                Console.WriteLine("\tYou must specify file name!");
+                return;
+            }
+
+            Console.Write("Connecting to {0}...\n\n", ftpUri);
+
+            FtpWebResponse response = null;
+
+            try
+            {
+                response = (FtpWebResponse)request.GetResponse();
+            }
+            catch (System.Net.WebException e)
+            {
+                Console.Write("Error: Couldn't find file {0} at {1}\nShow full message? (y/n) ", consoleArgs[0], ftpUri);
+
+                // if expception user can view one's messsage by typing 'y'
+                if ('y' == (char)Console.Read()) Console.Write("{0}\n", e);
+
+                Console.WriteLine();
+                return;
+            }
+
+            using (response)
+            {
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(responseStream))
+                    {
+                        // shows downloaded file on the screen
+                        Console.WriteLine(reader.ReadToEnd());
+                        Console.WriteLine("Download complete, status {0}", response.StatusDescription);
+                    }
+                }
+            }
+        }
+
+        public void GetCommandsHelp(params string[] consoleArgs)
+        {
+            using (StreamReader file = new StreamReader("../readme.txt"))
+            {
+                Console.WriteLine(file.ReadToEnd());
+            }
+        }
+
+        public void GetListDerictory(params string[] consoleArgs)
+        {
+            request = CreateFtpRequest(WebRequestMethods.Ftp.ListDirectoryDetails, ftpUri);
+            Console.Write("Connecting to {0}...\n\n", ftpUri);
+
+            FtpWebResponse response = null;
+
+            try
+            {
+                response = (FtpWebResponse)request.GetResponse();
+            }
+            catch (System.Net.WebException e)
+            {
+                Console.Write("Error: The remote name ftp://{0} couldn't be resolved\nShow full message? (y/n) ", ftpUri);
+
+                if ('y' == (char)Console.Read()) Console.Write("{0}\n", e);
+
+                Console.WriteLine();
+                return;
+            }
+
+            using (response)
+            {
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(responseStream))
+                    {
+                        // dictionary for this func keys
+                        Dictionary<string, ItemAttributes> arguments = new Dictionary<string, ItemAttributes>();
+                        // separate response on lines
+                        string[] directoryItemsNotSeparated = reader.ReadToEnd().Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+                        // then separate lines on items and place into a string array
+                        string[,] directoryItems = new string[directoryItemsNotSeparated.Length, 9];
+
+                        // add keys to dictionary
+                        arguments.Add("-ds", ItemAttributes.DayStamp);
+                        arguments.Add("-ms", ItemAttributes.MonthStamp);
+                        arguments.Add("-gn", ItemAttributes.GroupName);
+                        arguments.Add("-n", ItemAttributes.Name);
+                        arguments.Add("-on", ItemAttributes.OwnerName);
+                        arguments.Add("-rc", ItemAttributes.RefCount);
+                        arguments.Add("-s", ItemAttributes.Size);
+                        arguments.Add("-tys", ItemAttributes.TimeOrYearStamp);
+                        arguments.Add("-tm", ItemAttributes.TypeMod);
+
+                        // finally separate items on attributes
+                        for (int i = 0; i < directoryItems.GetLength(0); i++)
+                        {
+                            int j = 0;
+                            string[] directoryItem = directoryItemsNotSeparated[i].Split(new char[] { ' ' }, 9, StringSplitOptions.RemoveEmptyEntries);
+                            foreach (string attribute in directoryItem)
+                                directoryItems[i, j++] = attribute;
+                        }
+
+                        // display specified attributes
+                        if (0 == consoleArgs.Length || (1 == consoleArgs.Length && !arguments.ContainsKey(consoleArgs[0])))
+                        {
+                            Console.Write("Last modified\tSize\tName\n\n");
+                            for (int i = 0; i < directoryItems.GetLength(0); i++)
+                            {
+                                Console.Write("{0} {1} {2}\t{3}\t{4}", directoryItems[i, (int)ItemAttributes.DayStamp],
+                                                                       directoryItems[i, (int)ItemAttributes.MonthStamp],
+                                                                       directoryItems[i, (int)ItemAttributes.TimeOrYearStamp],
+                                                                       directoryItems[i, (int)ItemAttributes.Size],
+                                                                       directoryItems[i, (int)ItemAttributes.Name]);
+                                Console.WriteLine(directoryItems[i, (int)ItemAttributes.TypeMod][0] == '-' ? "" : "/");
+                            }
+                        }
+                        else
+                        {
+                            for (int i = 0; i < directoryItems.GetLength(0); i++)
+                            {
+                                for (int j = 0; j < consoleArgs.Length; j++)
+                                    if (arguments.ContainsKey(consoleArgs[j]))
+                                        Console.Write("{0}\t", directoryItems[i, (int)arguments[consoleArgs[j]]]);
+                                Console.WriteLine();
+                            }
+                        }
+                        Console.WriteLine("\nDirectory List Complete, status {0}", response.StatusDescription);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FtpConsoleClient/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace ftpConsoleClient
+{
+    class Program
+    {
+        private delegate void Command(params string[] consoleArguments);
+
+        // dictionary for console commands
+        static Dictionary<string, Command> commands;
+
+        // add new command in dictionary
+        static void DefineOperation(string cmd, Command body)
+        {
+            // if command already exists then throws exception
+            if (commands.ContainsKey(cmd))
+                throw new ArgumentException(string.Format("Operation {0} already exists", cmd), "op");
+            commands.Add(cmd, body);
+        }
+
+        static void Main(string[] args)
+        {
+            string uri, username, password;
+            string currentCommand;
+
+            Console.Write("Welcome to console ftp client!\n\nEnter ftp server you want to use (without ftp:// routine) (ftp.mozilla.org as default): ");
+            uri = Console.ReadLine();
+            Console.Write("Enter username: ");
+            username = Console.ReadLine();
+            Console.Write("Enter password: ");
+            password = Console.ReadLine();
+
+            FtpClient ftpClient = new FtpClient(uri, username, password);
+
+            // filling dictionary with commands
+            // first param is key=command, second param is func which processes this command
+            commands = new Dictionary<string, Command>
+            {
+                { "help", ftpClient.GetCommandsHelp },
+                { "exit", (dummy) => Environment.Exit(0) },
+            };
+            DefineOperation("ls", ftpClient.GetListDerictory);
+            DefineOperation("cat", ftpClient.DownloadAndShowFile);
+            DefineOperation("dl", ftpClient.DownloadAndSafeFile);
+            DefineOperation("cd", ftpClient.ChangeDirectory);
+
+            Console.WriteLine();
+            commands["ls"]();
+
+            // program main loop
+            // processes commands until 'exit' found
+            do
+            {
+                Console.Write("{0}>", ftpClient.FtpUri);
+                currentCommand = Console.ReadLine();
+
+                // separate command arguments
+                string[] currentCommandAndArguments = currentCommand.Trim().Split(new char[] { ' ' }, 2, StringSplitOptions.RemoveEmptyEntries);
+                string[] currentArguments = null;
+
+                // if command doesn't have arguments
+                try
+                {
+                    currentCommand = currentCommandAndArguments[0];
+                }
+                catch (System.IndexOutOfRangeException)
+                {
+                    currentCommand = "";
+                }
+
+                try
+                {
+                    currentArguments = currentCommandAndArguments[1].Trim().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                }
+                catch (System.IndexOutOfRangeException)
+                {
+                    currentArguments = new string[0];
+                }
+
+                // calls the appropriate func 
+                if (commands.ContainsKey(currentCommand))
+                    commands[currentCommand](currentArguments);
+            } while (currentCommand != "exit");
+
+            Console.ReadKey();
+        }
+    }
+}
