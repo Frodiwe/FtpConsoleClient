2e2e56c868cc22c260e0a87873b76ac4dbec4604
Fjodor Naumenko <ggrowling@gmail.com>
2015-02-10 01:10:45 +0300

    FTP methods factory implemented

---
+++ FtpConsoleClient/Infrastructure/AbstractFtpMethodCreator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ftpConsoleClient.Methods;
+
+namespace ftpConsoleClient.Infrastructure
+{
+    public abstract class AbstractFtpMethodCreator
+    {
+        public abstract AbstractFtpMethod Create();
+    }
+}
---
+++ FtpConsoleClient/Infrastructure/FtpMethodFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ftpConsoleClient.Methods;
+
+namespace ftpConsoleClient.Infrastructure
+{
+    public class FtpMethodFactory
+    {
+        protected Dictionary<string, AbstractFtpMethodCreator> factoryStorage;
+
+        public void Add<T>(string implementationId) where T : AbstractFtpMethod, new()
+        {
+            if (!factoryStorage.ContainsKey(implementationId))
+                factoryStorage.Add(implementationId, new FtpMethodCreator<T>());
+        }
+
+        public AbstractFtpMethod Create(string implementationId)
+        {
+            if (factoryStorage.ContainsKey(implementationId))
+                return factoryStorage[implementationId].Create();
+            else
+                return null;
+        }
+    }
+}
